a96c70ddcba10b48b7ae3ce990c83fbf5c14e389
Jorge Romanos <zebrv.apps@gmail.com>
2025-08-16 12:53:35 -0400

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	image: {
 		// Enable Astro's global responsive image styles
 		responsiveStyles: true,
-	}
+	},
+	site: 'https://appframes.app',
+	integrations: [sitemap()]
 });
